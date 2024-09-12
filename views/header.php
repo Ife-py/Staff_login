92aebd69f214638f671bb545e440fed5b88583fd
Ifeoluwa <omolekeifeoluwa2005@gmail.com>
2024-09-12 09:42:46 +0100

    making minimal changes

--- views/header.php
+++ views/header.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/Staff login/css/bootstrap.min (2).css">
-    <link rel="stylesheet" href="/Staff login/css/style.css">
+    <link rel="stylesheet" href="./css/bootstrap.min (2).css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Test</title>
 </head>
 <body>
